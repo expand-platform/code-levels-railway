924be53a3decd9c61095454efe2ce09320615dbf
Ca-tt <damir96.lukyanenko@gmail.com>
2026-04-08 16:24:16 +0300

    update jazzmin top menu and lesson objectives display

--- code_levels/settings/admin/jazzmin.py
+++ code_levels/settings/admin/jazzmin.py
@@ -2,6 +2,8 @@ JAZZMIN_SETTINGS_DICT = {
     "topmenu_links": [
         {"name": "Home", "url": "home"},
         {"name": "Projects", "url": "projects"},
+        {"name": "Topics", "url": "/dashboard/projects/?type=topic"},
+        {"name": "Courses", "url": "courses"},
     ],
     "custom_css": "css/admin/jazzmin.css",
     "custom_js": "js/admin/jazzmin.js"
