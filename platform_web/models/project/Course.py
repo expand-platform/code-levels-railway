dea06511e685409f03541db395dce34580093cba
Ca-tt <damir96.lukyanenko@gmail.com>
2026-06-03 15:14:22 +0300

    fix course-slug related bug

--- platform_web/models/project/Course.py
+++ platform_web/models/project/Course.py
@@ -3,16 +3,18 @@ import uuid
 from django.db import models
 from platform_web.models.project.ProgrammingLanguage import ProgrammingLanguage
 
+from platform_web.services.model.SlugService import SlugService
+
 
 class Course(models.Model):
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
-    
+
     languages = models.ManyToManyField(
         ProgrammingLanguage, related_name="courses", blank=True
     )
-    
+
     order = models.PositiveIntegerField(default=0)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
 
@@ -22,3 +24,13 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.title:
+            self.slug = SlugService.generate_unique_slug(
+                self.title,
+                self.__class__,
+                exclude_pk=self.pk,
+                fallback_prefix="course",
+            )
+        super().save(*args, **kwargs)
